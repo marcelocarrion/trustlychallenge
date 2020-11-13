22206aa521ecd259ee87af651039b58fa10aff55
Marcelo Carrion <marcelo.carrion@vixteam.com.br>
2020-11-13 02:15:06 -0300

    Initial commit (empty project)

---
+++ src/main/java/com/mcarrion/trustlychallenge/TrustlychallengeApplication.java
@@ -0,0 +1,13 @@
+package com.mcarrion.trustlychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustlychallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustlychallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcarrion/trustlychallenge/TrustlychallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcarrion.trustlychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustlychallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
